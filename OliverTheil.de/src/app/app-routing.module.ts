2aeaba064c284ee12a88480c1dc0499cae6b04cc
Olli T <92941044+OliverTheil@users.noreply.github.com>
2022-02-23 15:38:35 +0100

    013

--- OliverTheil.de/src/app/app-routing.module.ts
+++ OliverTheil.de/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataprotectionComponent } from './dataprotection/dataprotection.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 const routes: Routes = [
+  {path: 'imprint', component: ImprintComponent},
+  {path: 'dataprotection', component: DataprotectionComponent}
 ];
 
 @NgModule({
