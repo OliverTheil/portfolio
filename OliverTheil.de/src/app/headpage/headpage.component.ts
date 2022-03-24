0cf5bc2fe857caa30941fd4eb2858083bd253bf6
Olli T <92941044+OliverTheil@users.noreply.github.com>
2022-03-24 08:52:12 +0100

    037

--- OliverTheil.de/src/app/headpage/headpage.component.ts
+++ OliverTheil.de/src/app/headpage/headpage.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-headpage',
   templateUrl: './headpage.component.html',
-  styleUrls: ['./headpage.component.scss']
+  styleUrls: ['./headpage.component.scss'],
 })
 export class HeadpageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
