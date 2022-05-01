c5fc1eb5cf512136f6a1b910aea4ed244f830cab
Olli T <92941044+OliverTheil@users.noreply.github.com>
2022-05-01 08:44:50 +0200

    -

---
+++ portfolio/src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ImprintComponent } from './imprint/imprint.component';
+import { DataprotectionComponent } from './dataprotection/dataprotection.component';
+import { HeadpageComponent } from './headpage/headpage.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { SkillsComponent } from './skills/skills.component';
+import { PathComponent } from './path/path.component';
+import { ContactComponent } from './contact/contact.component';
+import { MainComponent } from './main/main.component';
+import { MoreaboutmeComponent } from './moreaboutme/moreaboutme.component';
+import { SocialComponent } from './social/social.component';
+import { OtherprojectsComponent } from './otherprojects/otherprojects.component';
+import { ConfirmpageComponent } from './confirmpage/confirmpage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProjectsComponent,
+    ImprintComponent,
+    DataprotectionComponent,
+    HeadpageComponent,
+    AboutmeComponent,
+    SkillsComponent,
+    PathComponent,
+    ContactComponent,
+    MainComponent,
+    MoreaboutmeComponent,
+    SocialComponent,
+    OtherprojectsComponent,
+    ConfirmpageComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ portfolio/src/app/confirmpage/confirmpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmpage',
+  templateUrl: './confirmpage.component.html',
+  styleUrls: ['./confirmpage.component.scss']
+})
+export class ConfirmpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
