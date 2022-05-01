3e041164a0b782bd01125ecd37334db190f7016b
Olli T <92941044+OliverTheil@users.noreply.github.com>
2022-05-01 08:45:25 +0200

    -

---
+++ portfolio/src/app/moreaboutme/moreaboutme.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-moreaboutme',
+  templateUrl: './moreaboutme.component.html',
+  styleUrls: ['./moreaboutme.component.scss']
+})
+export class MoreaboutmeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
