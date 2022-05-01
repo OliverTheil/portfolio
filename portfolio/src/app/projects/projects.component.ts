07ce4b1d1fa2c78618f7b64fe4c421b7b8b44071
Olli T <92941044+OliverTheil@users.noreply.github.com>
2022-05-01 10:51:09 +0200

    design fixes

--- portfolio/src/app/projects/projects.component.ts
+++ portfolio/src/app/projects/projects.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
+  jsClicked = false;
+  angularClicked = false;
   constructor() {}
 
   ngOnInit(): void {}
